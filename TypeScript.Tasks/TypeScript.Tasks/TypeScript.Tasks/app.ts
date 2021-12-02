375937df2b8c955b789e23cc125537e8b187abc6
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-12-02 17:36:58 +0300

    ReduceSummaryExample

--- TypeScript.Tasks/TypeScript.Tasks/TypeScript.Tasks/app.ts
+++ TypeScript.Tasks/TypeScript.Tasks/TypeScript.Tasks/app.ts
@@ -1,6 +1,10 @@
 ﻿import { ReduceExample } from "./reduceExample";
+import { ReduceSummaryExample } from "./reduceSummaryExample";
 
 let reduceExample = new ReduceExample();
 reduceExample.execute();
+
+let reduceSummaryExample = new ReduceSummaryExample();
+reduceSummaryExample.execute();
 
 setTimeout(x => { }, 10000);
---
+++ TypeScript.Tasks/TypeScript.Tasks/TypeScript.Tasks/reduceSummaryExample.ts
@@ -0,0 +1,12 @@
+﻿export class ReduceSummaryExample {
+    execute() {
+        const values = [753, 643, 975, 615];
+        console.log(JSON.stringify(values));
+
+        const sum = values => values.reduce((output, curVal) => output + curVal);
+        console.log(JSON.stringify(sum(values)));
+
+        const sum2 = values => values.reduceRight((output, curVal) => output + curVal);
+        console.log(JSON.stringify(sum2(values)));
+    }
+}
