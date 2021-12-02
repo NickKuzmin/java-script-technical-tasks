227eac0ea1ae5ff4f66889ebd33f3338c2073b90
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-12-02 17:42:50 +0300

    LodashGetExample

--- TypeScript.Tasks/TypeScript.Tasks/TypeScript.Tasks/app.ts
+++ TypeScript.Tasks/TypeScript.Tasks/TypeScript.Tasks/app.ts
@@ -1,10 +1,14 @@
 ﻿import { ReduceExample } from "./reduceExample";
 import { ReduceSummaryExample } from "./reduceSummaryExample";
+import { LodashGetExample } from "./lodashGetExample";
 
 let reduceExample = new ReduceExample();
 reduceExample.execute();
 
 let reduceSummaryExample = new ReduceSummaryExample();
 reduceSummaryExample.execute();
+
+let lodashGetExample = new LodashGetExample();
+lodashGetExample.execute();
 
 setTimeout(x => { }, 10000);
---
+++ TypeScript.Tasks/TypeScript.Tasks/TypeScript.Tasks/lodashGetExample.ts
@@ -0,0 +1,20 @@
+﻿export class LodashGetExample {
+    execute() {
+        const data = {
+            user: {
+                fullName: {
+                    firstName: 'Lev',
+                    lastName: 'Tolstoy',
+                }
+            }
+        };
+
+        const get = (path, obj) => path
+            .split('.')
+            .reduce((o, k) => o && o[k], obj);
+
+        console.log(get('user.fullName.firstName', data));
+        console.log(get('user.fullName.lastName', data));
+        console.log(get('user.firstBook.title', data));
+    }
+}
